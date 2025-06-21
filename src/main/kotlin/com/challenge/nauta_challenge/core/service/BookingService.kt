504eba1c714f1627edc38848f87bca2b47327256
Fran Castillo <castillofranciscodaniel@gmail.com>
2025-06-20 23:35:43 -0300

    test BookingServiceTest and service

--- src/main/kotlin/com/challenge/nauta_challenge/core/service/BookingService.kt
+++ src/main/kotlin/com/challenge/nauta_challenge/core/service/BookingService.kt
@@ -3,6 +3,7 @@ package com.challenge.nauta_challenge.core.service
 import com.challenge.nauta_challenge.core.model.Booking
 import com.challenge.nauta_challenge.core.repository.BookingRepository
 import com.challenge.nauta_challenge.core.repository.ContainerRepository
+import com.challenge.nauta_challenge.core.repository.InvoiceRepository
 import com.challenge.nauta_challenge.core.repository.OrderRepository
 import org.springframework.stereotype.Service
 
@@ -11,7 +12,8 @@ class BookingService(
     private val bookingRepository: BookingRepository,
     private val containerRepository: ContainerRepository,
     private val orderRepository: OrderRepository,
-    private val userLoggedService: UserLoggedService
+    private val userLoggedService: UserLoggedService,
+    private val invoiceRepository: InvoiceRepository
 ) {
 
     suspend fun saveBooking(booking: Booking): Booking {
@@ -22,7 +24,18 @@ class BookingService(
             booking.bookingNumber, userLogged.id!!
         ) ?: bookingRepository.save(booking)
 
+        val containersSaved = booking.containers.map { containerRepository.save(it) }
 
-        return bookingSaved
+        val ordersSaved = booking.orders.map { order ->
+            val orderSaved = orderRepository.save(order)
+            val invoicesSaved = order.invoices.map {
+                val invoiceWithOrderId = it.copy(orderId = orderSaved.id)
+                invoiceRepository.save(invoiceWithOrderId)
+            }
+            orderSaved.copy(invoices = invoicesSaved)
+        }
+
+
+        return bookingSaved.copy(containers = containersSaved, orders = ordersSaved)
     }
 }
